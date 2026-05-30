b1d43545d65e14290864325ffeb1d40fc20841b2
hakanyavuzz47-creator <hakanyavuzz47@gmail.com>
2026-05-30 13:44:25 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function surpriz() {
+    document.body.innerHTML = `
+        <h1>🎉 İYİ Kİ DOĞDUN ZİNNUR 🎉</h1>
+        <h2>❤️ Nice mutlu yıllara ❤️</h2>
+        <p>Umarım tüm dileklerin gerçek olur.</p>
+    `;
+}
