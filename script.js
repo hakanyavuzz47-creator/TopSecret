acfe609a7bdbe910b0e8ff18a05ac6e34fe29a66
hakanyavuzz47-creator <hakanyavuzz47@gmail.com>
2026-05-30 13:55:28 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,29 @@
 function surpriz() {
-    document.body.innerHTML = `
-        <h1>🎉 İYİ Kİ DOĞDUN ZİNNUR 🎉</h1>
-        <h2>❤️ Nice mutlu yıllara ❤️</h2>
-        <p>Umarım tüm dileklerin gerçek olur.</p>
-    `;
+  const muzik = document.getElementById("muzik");
+  muzik.play();
+
+  for (let i = 0; i < 40; i++) {
+    const kalp = document.createElement("div");
+    kalp.innerHTML = "❤️";
+    kalp.style.position = "fixed";
+    kalp.style.left = Math.random() * 100 + "vw";
+    kalp.style.top = "-30px";
+    kalp.style.fontSize = Math.random() * 25 + 20 + "px";
+    kalp.style.animation = "dus 3s linear forwards";
+    document.body.appendChild(kalp);
+
+    setTimeout(() => {
+      kalp.remove();
+    }, 3000);
+  }
 }
+
+const style = document.createElement("style");
+style.innerHTML = `
+@keyframes dus {
+  to {
+    transform: translateY(110vh) rotate(360deg);
+    opacity: 0;
+  }
+}`;
+document.head.appendChild(style);
